ce8237d0cf4bd86b7d221f888c49eff276083636
riyaznnb <riyaznnb@gmail.com>
2020-01-04 19:01:11 +0530

    [Riyazuddin]add .register component

--- ios/fundoonoteTests/fundoonoteTests.m
+++ ios/fundoonoteTests/fundoonoteTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface fundooNotesTests : XCTestCase
+@interface fundoonoteTests : XCTestCase
 
 @end
 
-@implementation fundooNotesTests
+@implementation fundoonoteTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,9 +1,7 @@
-import React from 'react-native'
-import { View, TextInput, Button} from 'react-native'
+import React,{Component} from 'react'
+import { View, TextInput, Button,TouchableHighlight} from 'react-native'
 import StyleSheet from '../styleSheets'
-import Snackbar from 'react-native-snackbar';
-import { TouchableHighlight } from 'react-native-gesture-handler';
-export default class Register extends React.component{
+export default class Register extends Component{
     constructor(props) {
         super(props);
         this.state = {
@@ -29,58 +27,27 @@ export default class Register extends React.component{
                 </View>
                 <View>
                   <TextInput
-                    id="firstName"
-                    fullWidth
-                    label="FirstName"
-                    type="text"
-                    margin="normal"
-                    variant="outlined"
-                    name="firstName"
                     value={this.state.firstName}
                     onChangeText={(firstName) => this.setState({firstName})}
                   />
                   <TextInput
-                    id="lastName"
-                    fullWidth
-                    label="lastName"
-                    type="text"
-                    name="lastName"
-                    margin="normal"
-                    variant="outlined"
                     value={this.state.lastName}
                     onChangeText={(lastName) => this.setState({lastName})}
                   />
                   <TextInput
-                    id="email"
-                    fullWidth
-                    label="Email"
-                    type="email"
-                    name="email"
-                    autoComplete="off"
-                    margin="normal"
-                    variant="outlined"
                     value={this.state.email}
                     onChangeText={(email) => this.setState({email})}
                   />   
                   <TextInput
-                    id="password"
-                    fullWidth
-                    label="Password"
-                    type="password"
-                    name="password"
-                    margin="normal"
-                    variant="outlined"
                     value={this.state.password}
                     onChangeText={(password) => this.setState({password})}
                   />
                   </View>
                 <View>
-                  <Button onClick={this.handleSubmit} variant="contained" color="primary">
-                    submit
-                </Button>
+                  <Button onClick={this.handleSubmit} title="Register"/>
                 </View>
                 <View>
-                  <TouchableHighlight>Already have an account?  Login</TouchableHighlight>
+                  <TouchableHighlight title="Already have an account?  Login"/>link
                 </View>
               </Card>
           </View>
