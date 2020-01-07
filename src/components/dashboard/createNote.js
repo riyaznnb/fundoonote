b8b98a94c5368347f0e4fbc2736bb21c424ed5df
riyaznnb <riyaznnb@gmail.com>
2020-01-07 19:27:46 +0530

    [Riyazuddin]Refactor .CreateNote component

--- src/components/dashboard/createNote.js
+++ src/components/dashboard/createNote.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, TextInput,Image } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, Image,Modal,TouchableHighlight } from 'react-native'
 import StyleSheet from '../../styleSheets'
 import { Card, Avatar } from 'react-native-elements'
 import Icon from 'react-native-vector-icons/Feather'
@@ -10,51 +10,59 @@ export default class CreateNote extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            pinned: false
+            pinned: false,
+            reminderOpen:false
         }
     }
     handlePinned = () => {
-        this.setState({pinned:!this.state.pinned})
+        this.setState({ pinned: !this.state.pinned })
+    }
+    reminderModalOpen = () => {
+        this.setState({reminderOpen:true})
+    }
+    reminderModalClose = () => {
+        this.setState({reminderOpen:false})
     }
 
     render() {
         return (
             <View style={StyleSheet.createNoteContainer}>
                 <View style={StyleSheet.createNoteHeaderInput}>
-                        <View style={StyleSheet.createNoteHeader}>
-                            <View>
-                                <View style={StyleSheet.createNoteItem}>
-                                    <TouchableOpacity>
-                                        <Icon name="arrow-left" size={30} color="black"/>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                            <View style={StyleSheet.createNoteHeaderRight}>
-                                <View style={StyleSheet.createNoteItem}>
-                                <TouchableOpacity onPress={this.handlePinned}>
-                                    {!this.state.pinned ?
-                                        <Icons name="pushpino" size={30} color="black"/>
-                                        :
-                                        <Icons name="pushpin" size={30} color="black"/>
-                                    }
-                                    </TouchableOpacity>
-                                </View>
-                                <View style={StyleSheet.createNoteItem}>
-                                    <TouchableOpacity>
-                                        <IconM name="bell-plus" size={30} color="black"/>
-                                    </TouchableOpacity>
-                                </View>
-                                <View style={StyleSheet.createNoteItem}>
-                                    <TouchableOpacity>
-                                        <IconMaterial name="archive" size={30} color="black"/>
-                                    </TouchableOpacity>
-                                </View>
+                    <View style={StyleSheet.createNoteHeader}>
+                        <View>
+                            <View style={StyleSheet.createNoteItem}>
+                                <TouchableOpacity>
+                                    <Icon name="arrow-left" size={30} color="black" />
+                                </TouchableOpacity>
                             </View>
                         </View>
+                        <View style={StyleSheet.createNoteHeaderRight}>
+                            <View style={StyleSheet.createNoteItem}>
+                                <TouchableOpacity onPress={this.handlePinned}>
+                                    {!this.state.pinned ?
+                                        <Icons name="pushpino" size={30} color="black" />
+                                        :
+                                        <Icons name="pushpin" size={30} color="black" />
+                                    }
+                                </TouchableOpacity>
+                            </View>
+                            <View style={StyleSheet.createNoteItem}>
+                                <TouchableOpacity onPress={this.reminderModalOpen}>
+                                    <IconM name="bell-plus" size={30} color="black" />
+                                </TouchableOpacity>
+                            </View>
+                            <View style={StyleSheet.createNoteItem}>
+                                <TouchableOpacity>
+                                    <IconMaterial name="archive" size={30} color="black" />
+                                </TouchableOpacity>
+                            </View>
+                            
+                        </View>
+                    </View>
                     <View style={StyleSheet.createNoteInput}>
                         <View style={StyleSheet.createNoteItem}>
                             <TextInput
-                                 style={StyleSheet.createNoteTitle}
+                                style={StyleSheet.createNoteTitle}
                                 placeholder="Title" />
                         </View>
                         <View style={StyleSheet.createNoteItem}>
@@ -64,20 +72,20 @@ export default class CreateNote extends Component {
                                 placeholder="Note" />
                         </View>
                     </View>
-            </View>
-            <View style={StyleSheet.createNoteFooter}>
-                <View style={StyleSheet.createNoteItem}>
-                    <TouchableOpacity>
-                        <Icon name="plus-square" size={30} color="black"/>
-                    </TouchableOpacity>
                 </View>
-                <View style={StyleSheet.createNoteItem}>
-                    <TouchableOpacity>
-                        <Icon name="more-vertical" size={30} color="black"/>
-                    </TouchableOpacity>
+                <View style={StyleSheet.createNoteFooter}>
+                    <View style={StyleSheet.createNoteItem}>
+                        <TouchableOpacity>
+                            <Icon name="plus-square" size={30} color="black" />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={StyleSheet.createNoteItem}>
+                        <TouchableOpacity>
+                            <Icon name="more-vertical" size={30} color="black" />
+                        </TouchableOpacity>
+                    </View>
                 </View>
             </View>
-        </View>
         )
     }
 }
